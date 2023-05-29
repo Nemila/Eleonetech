e94fa098ffab5bfac51476de14428fa6c85c49fb
Lamine <pridila.2006@gmail.com>
2023-05-29 00:07:48 +0000

    remove usercontainer component

--- app/chats/layout.tsx
+++ app/chats/layout.tsx
@@ -2,7 +2,6 @@
 
 import type { User } from "@clerk/nextjs/dist/types/server";
 import { useEffect, useState } from "react";
-import UserContainer from "./user-container";
 import ChatPanel from "./(chat-panel)/chat-panel";
 
 type Props = {
